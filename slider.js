115e96a02231895dc374673bf5dbd7c02b284c3e
Shelley Bassett <m.bassett@y7mail.com>
2018-02-14 11:39:08 +1100

    using jquery. got button functionality

--- slider.js
+++ slider.js
@@ -1,12 +1,11 @@
-document.onreadystatechange = function () {
-    if (document.readyState == "interactive") { //Don't run scripts until the document has loaded
-
+$(document).ready(function(){ //Don't run scripts until the document has loaded
 	//Global Variables
 
-
-	//Button Functionality
-		
-
-
-	}
-}
+	//Button Functions
+	$('.next').on('click', function(){
+		console.log('next slide please');
+	});
+	$('.back').on('click', function(){
+		console.log('previous slide please');
+	});
+});
