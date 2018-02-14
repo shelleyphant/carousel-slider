1a024dd704179f3b35b61c118c37eab2385a6d4b
Shelley Bassett <m.bassett@y7mail.com>
2018-02-14 11:56:17 +1100

    set the slide function and the triggers

--- slider.js
+++ slider.js
@@ -7,15 +7,23 @@ $(document).ready(function(){ //Don't run scripts until the document has loaded
 	var counter = 0;
 
 	//Sliding Function
-	function slide(){
-		$()
+	function slide(direction){
+		$(slides.get(counter)).addClass('current');
+
+		slides.removeClass('current prev forth');
+		counter = counter + direction;
+
 	}
 
 	//Button Functions
 	$('.next').on('click', function(){
+		slide(1);
 		console.log('next slide please');
 	});
 	$('.back').on('click', function(){
+		slide(-1);
 		console.log('previous slide please');
 	});
+
+	slide(0);
 });
