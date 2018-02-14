3497178b17ea777960354d5fc3358a113540ffdd
Shelley Bassett <m.bassett@y7mail.com>
2018-02-14 11:15:49 +1100

    set up js and added css notes

---
+++ slider.js
@@ -0,0 +1,12 @@
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") { //Don't run scripts until the document has loaded
+
+	//Global Variables
+
+
+	//Button Functionality
+		
+
+
+	}
+}
