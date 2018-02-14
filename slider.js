f0d4a0584e180980c5284309d572b51a43bebe8d
Shelley Bassett <m.bassett@y7mail.com>
2018-02-14 12:05:41 +1100

    got the slides changing position

--- slider.js
+++ slider.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){ //Don't run scripts until the document has loaded
 	
 	//Global Variables
-	var slides = $('.slider ol li');
+	var slides = $('.carousel ol li');
 	var current = slides[0];
 	var number = slides.length;
 	var counter = 0;
@@ -12,6 +12,21 @@ $(document).ready(function(){ //Don't run scripts until the document has loaded
 
 		slides.removeClass('current prev forth');
 		counter = counter + direction;
+
+		if(direction === -1 && counter < 0){
+			counter = number - 1;
+		}
+		if(direction === 1 && !slides[counter]){
+			counter = 0;
+		}
+
+		$(slides.get(counter)).addClass('current');
+		$(slides.get(counter - 1)).addClass('prev');
+		$(slides.get(counter + 1)).addClass('forth');
+
+		if( (counter + 1) === number){
+			$(slides.get(0)).addClass('forth');
+		}
 
 	}
 
