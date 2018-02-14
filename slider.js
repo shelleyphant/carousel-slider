5f1c70f6813fe4a0d354ca6313abc229581c2933
Shelley Bassett <m.bassett@y7mail.com>
2018-02-14 11:44:41 +1100

    set the global variables

--- slider.js
+++ slider.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){ //Don't run scripts until the document has loaded
+	
 	//Global Variables
+	var slides = $('.slider ol li');
+	var current = slides[0];
+	var number = slides.length;
+	var counter = 0;
 
 	//Button Functions
 	$('.next').on('click', function(){
